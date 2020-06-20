4c6aab44ec6736ed11e96181fe83c28f3106452a
V <maja5225@hotmail.com>
2020-06-20 13:56:26 +0200

    code cleanup, style changes

--- script/main.ts
+++ script/main.ts
@@ -1,7 +1,7 @@
 //the class Location is already defined in "lib": "dom", that is why this documents Location = MyLocation
 let locations: Array<MyLocation> = [];
 
-declare var sortOrder: string;
+declare var sortOrder: string; //declared in their respective html pages
 
 class MyLocation {
     locationName: string;
@@ -35,7 +35,11 @@ class MyLocation {
                             <hr>
                         </div>
                         <div>
-                            <p class="card-text text-right bottom-text"><small class="text-muted">Created: ${this.dateTimeCreated.toLocaleString("en-DE", options)}</small></p>
+                            <p class="card-text text-right bottom-text">
+                                <small class="text-muted">Created: 
+                                    ${this.dateTimeCreated.toLocaleString("en-DE", options)}
+                                </small>
+                            </p>
                         </div>
                     </div>
                 </div>
@@ -76,7 +80,11 @@ class Restaurant extends MyLocation {
                         </ul>
                     </div>
                     <div>
-                        <p class="card-text text-right bottom-text"><small class="text-muted">Created: ${this.dateTimeCreated.toLocaleString("en-DE", options)}</small></p>
+                        <p class="card-text text-right bottom-text">
+                            <small class="text-muted">
+                                Created: ${this.dateTimeCreated.toLocaleString("en-DE", options)}
+                            </small>
+                        </p>
                     </div>
                 </div>
             </div>
@@ -99,7 +107,7 @@ class Events extends MyLocation {
 
     display() {
         var options = { weekday: "short", year: "numeric", month: "long", day: "numeric" };
-        var optionsTime = {hour: "2-digit", minute: "2-digit"};
+        var optionsTime = { hour: "2-digit", minute: "2-digit" };
         return `
         <div class="col-md-6 col-lg-3 mb-4 d-md-flex align-items-md-stretch">
             <div class="card shadow-sm">
@@ -119,7 +127,11 @@ class Events extends MyLocation {
                         </ul>
                     </div>
                     <div>
-                        <p class="card-text text-right bottom-text"><small class="text-muted">Created: ${this.dateTimeCreated.toLocaleString("en-DE", options)}</small></p>
+                        <p class="card-text text-right bottom-text">
+                            <small class="text-muted">
+                                Created: ${this.dateTimeCreated.toLocaleString("en-DE", options)}
+                            </small>
+                        </p>
                     </div>
                 </div>
             </div>
@@ -129,28 +141,28 @@ class Events extends MyLocation {
 }
 
 locations.push(new MyLocation("St. Charles Church", "Karlsplartz 1", 1010, "Vienna", "media/loc-karlsplatz.jpg", new Date(2019, 10, 24)));
-locations.push(new MyLocation("Schönbrunn Park", "Maxingstraße 13b", 1130, "Wien", "media/loc-schoenbrunn.jpg", new Date(2019, 12, 5)));
+locations.push(new MyLocation("Schönbrunn Park", "Maxingstraße 13b", 1130, "Vienna", "media/loc-schoenbrunn.jpg", new Date(2019, 12, 5)));
 locations.push(new Restaurant("ON Restaurant", "Wehrgasse 8", 1050, "Vienna", "media/locrest-on.jpg", new Date(2020, 1, 15), "+43(1)5854900", "Chinese", "http://www.restaurant-on.at/"));
-locations.push(new Restaurant("BioFrische", "Stutterheimstraße 6", 1150, "Wien", "media/locrest-biofrische.jpg", new Date(2020, 3, 24), "+43(1) 9529215", "Indian", "https://biofrische.wien/"));
-locations.push(new Events("Cats - the musical", "http://catsmusical.at", new Date(2020,12,15,20,0), "Ronacher- Seilerstätte 9", 1010, "Wien", 120.50,"media/loce-cats.jpg", new Date (2020,6,10)));
-locations.push(new Events("Guns N' Roses", "https://www.gunsnroses.com/", new Date(2020,6,9,19,30), "Ernst-Happel Stadion, Meierstraße 7", 1020, "Wien", 95.50, "media/loce-gunsnroses.jpg", new Date (2020,3,2)));
-locations.push(new MyLocation("Kahlenberg", "Am Kahlenberg 2-3", 1190, "Wien", "media/loc-kb.jpg", new Date(2018,8,12)));
-locations.push(new MyLocation("Haus des Meeres", "Fritz-Grünbaum-Platz 1", 1060, "Wien", "media/loc-hdm.jpg", new Date(2019,3,4)));
-locations.push(new MyLocation("Donauinsel", "Donauinsel", 1220, "Wien", "media/loc-donauinsel.jpg", new Date(2017,7,24)));
-locations.push(new MyLocation("Belvedere Palace", "Prinz Eugen-Straße 58", 1030, "Wien", "media/loc-belvedere.jpg", new Date(2016,4,11)));
-locations.push(new MyLocation("Hunderwasser House", "Kegelgasse 36-38", 1030, "Wien", "media/loc-hundertw.jpg", new Date(2015,9,1)));
-locations.push(new Events("Tool", "https://toolband.com/", new Date(2019,6,5,19,30), "Roland-Rainer-Platz 1", 1150, "Wien", 120, "media/loce-tool.jpg", new Date(2019,6,1)));
-locations.push(new Events ("Muse", "https://www.muse.mu/", new Date(2016,5,9,19,30), "Roland-Rainer-Platz 1", 1150, "Wien", 110, "media/loce-muse.jpg", new Date(2016,4,29)));
-locations.push(new Restaurant("Noble Savage", "Salzgries 15", 1010, "Wien", "media/locrest-noble.jpg", new Date(2019.11,10), "0664/99 49 83 89", "International", "https://www.noblesavage.at/"));
-locations.push(new Restaurant("Seidengasse 31", "Seidengasse 31", 1070, "Wien", "media/locrest-seidengasse.jpg", new Date(2020,1,5), "0677/637 57 940", "Mediterranean","")); //currently no webpage avaliable
-locations.push(new Restaurant("Sperling im Augarten", "Obere Augartenstraße 1", 1020, "Wien", "media/locrest-sperling.jpg", new Date(2019,12,11), "01/997 72 66", "austrian international","https://www.sperling.wien/"));
+locations.push(new Restaurant("BioFrische", "Stutterheimstraße 6", 1150, "Vienna", "media/locrest-biofrische.jpg", new Date(2020, 3, 24), "+43(1) 9529215", "Indian", "https://biofrische.Vienna/"));
+locations.push(new Events("Cats - the musical", "http://catsmusical.at", new Date(2020, 12, 15, 20, 0), "Ronacher- Seilerstätte 9", 1010, "Vienna", 120.50, "media/loce-cats.jpg", new Date(2020, 6, 10)));
+locations.push(new Events("Guns N' Roses", "https://www.gunsnroses.com/", new Date(2020, 6, 9, 19, 30), "Ernst-Happel Stadion, Meierstraße 7", 1020, "Vienna", 95.50, "media/loce-gunsnroses.jpg", new Date(2020, 3, 2)));
+locations.push(new MyLocation("Kahlenberg", "Am Kahlenberg 2-3", 1190, "Vienna", "media/loc-kb.jpg", new Date(2018, 8, 12)));
+locations.push(new MyLocation("Haus des Meeres", "Fritz-Grünbaum-Platz 1", 1060, "Vienna", "media/loc-hdm.jpg", new Date(2019, 3, 4)));
+locations.push(new MyLocation("Donauinsel", "Donauinsel", 1220, "Vienna", "media/loc-donauinsel.jpg", new Date(2017, 7, 24)));
+locations.push(new MyLocation("Belvedere Palace", "Prinz Eugen-Straße 58", 1030, "Vienna", "media/loc-belvedere.jpg", new Date(2016, 4, 11)));
+locations.push(new MyLocation("Hunderwasser House", "Kegelgasse 36-38", 1030, "Vienna", "media/loc-hundertw.jpg", new Date(2015, 9, 1)));
+locations.push(new Events("Tool", "https://toolband.com/", new Date(2019, 6, 5, 19, 30), "Roland-Rainer-Platz 1", 1150, "Vienna", 120, "media/loce-tool.jpg", new Date(2019, 6, 1)));
+locations.push(new Events("Muse", "https://www.muse.mu/", new Date(2016, 5, 9, 19, 30), "Roland-Rainer-Platz 1", 1150, "Vienna", 110, "media/loce-muse.jpg", new Date(2016, 4, 29)));
+locations.push(new Restaurant("Noble Savage", "Salzgries 15", 1010, "Vienna", "media/locrest-noble.jpg", new Date(2019.11, 10), "0664/99 49 83 89", "International", "https://www.noblesavage.at/"));
+locations.push(new Restaurant("Seidengasse 31", "Seidengasse 31", 1070, "Vienna", "media/locrest-seidengasse.jpg", new Date(2020, 1, 5), "0677/637 57 940", "Mediterranean", "")); //currently no webpage avaliable
+locations.push(new Restaurant("Sperling im Augarten", "Obere Augartenstraße 1", 1020, "Vienna", "media/locrest-sperling.jpg", new Date(2019, 12, 11), "01/997 72 66", "Austrian, international", "https://www.sperling.Vienna/"));
 
 
 if (sortOrder == "descending abc") {
     locations.sort((first, second) => second.locationName.localeCompare(first.locationName));
 } else if (sortOrder == "ascending abc") {
     locations.sort((first, second) => first.locationName.localeCompare(second.locationName));
-} else if  (sortOrder == "ascending") {
+} else if (sortOrder == "ascending") {
     locations.sort((first, second) => second.dateTimeCreated.getTime() - first.dateTimeCreated.getTime());
 } else if (sortOrder == "descending") {
     locations.sort((first, second) => first.dateTimeCreated.getTime() - second.dateTimeCreated.getTime());
